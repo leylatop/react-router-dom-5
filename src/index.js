b68d92a69603806de189f6a2731983928015a3a1
leyla_qiao <leyla_qiao@163.com>
2024-12-11 10:10:23 +0800

    add src/react-router-dom demo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import User from './components/User';
+import Profile from './components/Profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={Home} />
+        <Route path="/user" exact component={User} />
+        <Route path="/profile" exact component={Profile} />
+      </BrowserRouter>
+    </div>
   </React.StrictMode>
 );
